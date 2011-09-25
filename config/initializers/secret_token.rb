544af5d46f1ee7c64e49dc0126f158a1a2d03e97
Manoel de Albuquerque Lira Amaro <mano.lira@gmail.com>
2011-09-25 03:05:09 -0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Knowledgetv::Application.config.secret_token = '9be2bcf88e211eac67cbd6d8fce5c3385bd1db3adb57efd92b8a95a5d0710235b29235a8949528855b116a11aa01ec227645e3161c53a167a9e5ffec718c9c91'
---
+++ public/javascripts/application.js
@@ -0,0 +1,21 @@
+// This is a manifest file that'll be compiled into including all the files listed below.
+// Add new JavaScript/Coffee code in separate files in this directory and they'll automatically
+// be included in the compiled file accessible from http://example.com/assets/application.js
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+//= require jquery_new
+//= require jquery_ujs
+//= require ui_core
+//= require jquery_cycle_all_min
+//= require jquery_nivo_slider
+//= require jquery_easing
+//= require jquery_prettyPhoto
+//= require jquery_jcarousel_min
+//= require jquery_tipsy
+//= require jquery_arrowFade
+//= require ui_tabs
+//= require jquery_custom                 
+//= require jquery_tweetable 
+//= require cufon-yui
+//= require waukegan_font
