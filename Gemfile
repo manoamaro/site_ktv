5a2307791d08bf4187f5fc681d2e2174461b2e69
Manoel de Albuquerque Lira Amaro <mano.lira@gmail.com>
2011-10-14 13:25:22 -0300

    inploy added

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rails', '3.0.10'
 
 gem 'sqlite3'
 gem 'mysql'
+gem 'inploy', :require => false
 # Use unicorn as the web server
 # gem 'unicorn'
 
---
+++ config/deploy.rb
@@ -0,0 +1,15 @@
+application = "knowledgetv"
+repository = 'git://github.com/manoamaro/site_ktv.git'
+hosts = ['knowledgetv.lavid.ufpb.br']
+
+path = '/var/local/apps'                      # default /var/local/apps
+user = 'knowledgetv'                          # default deploy
+#ssh_opts = '-A'                              # default empty
+#branch = 'new_version'                       # default master
+environment = 'development'                      # default production
+#sudo = true                                  # default false
+#cache_dirs = ['public/cache', 'tmp/cache']   # default ['public/cache']
+#skip_steps = ['install_gems', 'clear_cache'] # default []
+#pp_folder = 'project_folder'                # default empty
+#login_shell = true                           # default false
+#bundler_path = 'another_path'                # default ~/.bundle
